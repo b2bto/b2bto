4ab3b41c6560c4f6068fdabde2d68b9014d55379
Luis Rojas <luisrojasrincon@gmail.com>
2021-12-23 17:53:06 -0500

    Initial commit for avility clicle four

---
+++ src/main/java/com/website/firstwebsaite/FirstwebsaiteApplication.java
@@ -0,0 +1,13 @@
+package com.website.firstwebsaite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstwebsaiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstwebsaiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/website/firstwebsaite/FirstwebsaiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.website.firstwebsaite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstwebsaiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
